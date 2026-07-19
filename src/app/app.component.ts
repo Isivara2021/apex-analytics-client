820f99931184adde80a13367d8c9ff0bd3cf5367
Isivara2021 <isivara2021@gmail.com>
2026-07-19 23:19:45 +0530

    Hide navbar on auth pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,40 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AuthService } from './services/auth.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CommonModule, NavbarComponent],
   template: `
-    <app-navbar *ngIf="authService.isLoggedIn()"></app-navbar>
+    <app-navbar *ngIf="showNavbar"></app-navbar>
     <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit {
-  constructor(public authService: AuthService, private renderer: Renderer2) {}
+  showNavbar = false;
+
+  private readonly AUTH_ROUTES = ['/login', '/register'];
+
+  constructor(
+    public authService: AuthService,
+    private renderer: Renderer2,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     const savedTheme = localStorage.getItem('theme') || 'light';
     this.renderer.setAttribute(document.documentElement, 'data-theme', savedTheme);
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      const currentUrl = event.urlAfterRedirects || event.url;
+      const isAuthRoute = this.AUTH_ROUTES.some(route => currentUrl.startsWith(route));
+      this.showNavbar = !isAuthRoute && this.authService.isLoggedIn();
+    });
   }
 }
